6bd55e4ce44e741346e5cf390e349d97541bc7a2
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:19:44 +0500

    Add files via upload

---
+++ q7.ts
@@ -0,0 +1,10 @@
+/* Ques # 07:
+Number Eight: Write Addition, Subtraction, Multiplication and Division operations that each result in the number 8,Be sure to enclose your operations in print statements to see the results.*/
+
+
+
+
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
